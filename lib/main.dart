9135c4a653dc3b76ce8f28520239376c099d984d
mehmet <mdemir85@icloud.com>
2021-11-15 07:50:19 +0300

    quran package url pointed to git

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:pixel_perfect/pixel_perfect.dart';
-import 'package:quran_app/screens/home/home_screen.dart';
 import 'package:quran_app/screens/onboarding/onboarding_screen.dart';
-import 'package:quran_app/screens/recite/recite_screen.dart';
 import 'package:quran_app/theme/theme_data.dart';
 
 void main() {
@@ -22,8 +19,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: lightTheme,
         // home: ReciteScreen(surahNumber: 1),
-        home: HomeScreen(),
-        // home: OnBoardingScreen(),
+        // home: HomeScreen(),
+        home: OnBoardingScreen(),
 
         // PixelPerfect(
         //   // assetPath: 'assets/image/splash.jpeg',
